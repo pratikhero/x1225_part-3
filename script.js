24ce61dae481b75f23ee470fcd4538b072731f82
pratikhero <105071990+pratikhero@users.noreply.github.com>
2024-06-11 14:16:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to the video section
+document.getElementById('video-section').addEventListener('click', function() {
+    // Play the video when the section is clicked
+    document.querySelector('video').play();
+});
